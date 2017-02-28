2805d92960d3253b346dff77ca4e72fcfdd9f180
Villy G <villyg@icloud.com>
2017-02-27 20:00:46 -0500

    Initial Commit

---
+++ TableView-FullScreenSectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView-FullScreenSectionView
+//
+//  Created by Veliyan Georgiev on 2/27/17.
+//  Copyright © 2017 VillyG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
